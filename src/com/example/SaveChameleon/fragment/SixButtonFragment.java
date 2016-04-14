3a74aa122659f049c41fb5e0b851ab3992c52a22
chendongmidong <445397734@qq.com>
2016-04-14 23:19:32 +0800

    UI

---
+++ src/com/example/SaveChameleon/fragment/SixButtonFragment.java
@@ -0,0 +1,19 @@
+package com.example.SaveChameleon.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.SaveChameleon.R;
+
+/**
+ * Created by zhao on 2016/4/14.
+ */
+public class SixButtonFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.six_button,container,false);
+        return view;
+    }
+}
